e6e44dba3f20ad6732452d7a8290492d0e992030
Joshua-Rose <joshua.rose1@outlook.com>
2020-03-31 08:57:45 -0500

    converting from ul to table

--- src/components/Issues/Issues.tsx
+++ src/components/Issues/Issues.tsx
@@ -9,14 +9,40 @@ interface Props {
 }
 const className = 'IssueList'
 
-const Issues = ({data}: Props) => {
+const Issues = ({ data }: Props) => {
     return (
         <div className={className}>
             <h3>Issues</h3>
-            <ul>
-                {data.repository?.issues && data.repository?.issues.nodes?.map((node, i)=>(<li key={i} >{node?.title}</li>))}
-
-            </ul>
+            {
+                data.repository?.issues ? (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Issue Title</th><th>Labels</th><th>Date Opened</th><th>Date Updated</th><th>Id</th><th>url</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.repository.issues.nodes?.map(issue => (
+                                    <tr>
+                                        {[
+                                            issue?.title,
+                                            issue?.labels?.nodes?.map(label=>label?.name).join(';'),
+                                            issue?.createdAt,
+                                            issue?.updatedAt,
+                                            issue?.number,
+                                            issue?.url,
+                                        ].map(value => (<td>{value}</td>))
+                                        }
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                ) : (
+                        <div className="no_issues">No Issues found</div>
+                    )
+            }
         </div>
     )
 }
